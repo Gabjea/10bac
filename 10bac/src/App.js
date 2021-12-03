9d27b11adb388d565d2a2f6e5d9eddf1c42fe7bb
robertfedus <robertfedus22@gmail.com>
2021-12-03 21:52:03 +0200

    Subjects page

--- 10bac/src/App.js
+++ 10bac/src/App.js
@@ -1,23 +1,26 @@
-import "./App.css";
-import Nav from "./../src/routes/global/Nav/Nav";
-import { Routes, Route } from "react-router-dom";
-import Root from "./routes/Root/Root";
-import Register from "./routes/Register/Register";
-import Login from "./routes/Login/Login";
+import './App.css';
+import Nav from './../src/routes/global/Nav/Nav';
+import { Routes, Route } from 'react-router-dom';
+import Root from './routes/Root/Root';
+import Register from './routes/Register/Register';
+import Login from './routes/Login/Login';
 
 export default function App() {
-	return (
-		<div className="App">
-			<Nav />
-			<Routes>
-				<Route exact path="/" element={<Root />} />
-				<Route path="/subiecte" element={<Root />} />
-				<Route path="/materii" element={<Root />} />
-				<Route path="/profil" element={<Root />} />
-				<Route path="/istoric" element={<Root />} />
-				<Route path="/inregistrare" element={<Register />} />
-				<Route path="/autentificare" element={<Login />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav />
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Root />} />
+          <Route path="/materii" element={<Root />} />
+
+          <Route path="/subiecte" element={<Root />} />
+          <Route path="/profil" element={<Root />} />
+          <Route path="/istoric" element={<Root />} />
+          <Route path="/inregistrare" element={<Register />} />
+          <Route path="/autentificare" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
