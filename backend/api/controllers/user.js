964189412e8d1cb2498873651425e92889cfa074
Gabjea <43420432+Gabjea@users.noreply.github.com>
2021-12-03 17:07:07 +0200

    encrypt user password in db

--- backend/api/controllers/user.js
+++ backend/api/controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../../models/user')
 const mongoose = require("../../database")
-
+const bcrypt = require('bcrypt');
 const loginController = async (req,res ) => {
 
 
@@ -17,14 +17,18 @@ const registerController = async (req,res ) => {
     if (alreadyExistsUser) {
         return res.status(406).json({ message: "Acest email este deja folosit!" });
       }
+    const passwordSalt = 10
+    bcrypt.hash(password, passwordSalt , async function(err, hashedPassword) {
+        
+        const newUser = new User({_id: new mongoose.Types.ObjectId ,username,email,password:hashedPassword, role:"user" });
+        const savedUser = await newUser.save().catch((err) => {
+            console.log("Error: ", err);
+            res.status(500).json({ error: "Inregistrarea a esuat!" });
+        });
+  
+        if (savedUser) res.json({ message: "Te-ai inregistrat cu succes" });
+    });
     
-    const newUser = new User({_id: new mongoose.Types.ObjectId ,username,email,password, role:"user" });
-    const savedUser = await newUser.save().catch((err) => {
-        console.log("Error: ", err);
-        res.status(500).json({ error: "Inregistrarea a esuat!" });
-      });
-    
-      if (savedUser) res.json({ message: "Te-ai inregistrat cu succes" });
 }
 
 module.exports = {
