751e8d83939af158d6d61c7b8ee136bf677f96a2
StanaAndrei <stadey33@gmail.com>
2021-12-04 23:48:04 +0200

    history finished

--- 10bac/src/routes/History/History.jsx
+++ 10bac/src/routes/History/History.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import globalVars from "./../../globalVars";
+import { getCookie } from "./../../utils";
 
 export default function History() {
-    return (
-        <div>
-            <h1>History</h1>
-            
-        </div>
-    )
+	const [notes, setNotes] = React.useState(null);
+
+	React.useEffect(() => {
+		const url = `${globalVars.apiPrefix}/user/note`;
+		axios.get(url, {
+			headers: {
+				Authorization: getCookie("jwt"),
+			},
+		}).then(res => {
+            setNotes(res.data);
+        }, err => {
+            alert('error');
+            console.error(err);
+        })
+	}, []);
+
+	return (
+		<div>
+			<h1>History</h1>
+			<br />
+			{notes && notes.map(note => {
+                return (
+                    <div>
+                        <hr />
+                            <p>test:{note.test_title}</p>
+                            <p>nota:{note.nota}</p>
+                            <p>data:{note.date}</p>
+                        <hr />
+                    </div>
+                );
+            })}
+		</div>
+	);
 }
