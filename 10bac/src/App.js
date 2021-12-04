7d23e91b41a3dfc2d2272a3fc31a74e0b73e260b
robertfedus <robertfedus22@gmail.com>
2021-12-04 11:53:46 +0200

    Modificari mici

--- 10bac/src/App.js
+++ 10bac/src/App.js
@@ -7,23 +7,23 @@ import Login from './routes/Login/Login';
 import Profile from './routes/Profile/Profile';
 import Subject from './routes/Subject/Subject';
 import Lessons from './routes/Lessons/Lessons';
+import { isLoggedIn } from './utils';
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
-      <div className="content">
+      <div className="content" style={{ width: isLoggedIn() ? '83%' : '100%' }}>
         <Routes>
           <Route exact path="/" element={<Root />} />
           <Route path="/materii" element={<Root />} />
           <Route path="/lectii/:id_materie/:id_capitol" element={<Lessons />} />
           <Route path="/materie/:id" element={<Subject />} />
           <Route path="/subiecte" element={<Root />} />
-          <Route path="/profil" element={<Root />} />
           <Route path="/istoric" element={<Root />} />
           <Route path="/inregistrare" element={<Register />} />
           <Route path="/autentificare" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profil" element={<Profile />} />
         </Routes>
       </div>
     </div>
