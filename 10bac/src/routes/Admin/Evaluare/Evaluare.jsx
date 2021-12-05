f1fa4c7da9bdfe746c90087a4f2a34203f1e516a
robertfedus <robertfedus22@gmail.com>
2021-12-05 12:09:02 +0200

    Design Stripe

--- 10bac/src/routes/Admin/Evaluare/Evaluare.jsx
+++ 10bac/src/routes/Admin/Evaluare/Evaluare.jsx
@@ -1,257 +1,311 @@
-import React from "react";
-import globalVars from "./../../../globalVars";
-import { getCookie } from "../../../utils";
-import axios from "axios";
+import React from 'react';
+import './Evaluare.css';
+import globalVars from './../../../globalVars';
+import { getCookie } from '../../../utils';
+import axios from 'axios';
 const qLen = 9;
 
 export default function Evaluare() {
-	const handleFormSubmit = (event) => {
-		console.log("hail!");
-		event.preventDefault();
-		const materieElem = document.querySelector("#materie_id");
-		const capitolElem = document.querySelector("#capitol_id");
-		const quizNameElem = document.querySelector("#nume-quiz");
+  const handleFormSubmit = event => {
+    // console.log("hail!");
+    event.preventDefault();
+    const materieElem = document.querySelector('#materie_id');
+    const capitolElem = document.querySelector('#capitol_id');
+    const quizNameElem = document.querySelector('#nume-quiz');
 
-		if (!materieElem.value || !capitolElem.value || !quizNameElem.value) {
-			alert("smth is null!");
-			return;
-		}
+    if (!materieElem.value || !capitolElem.value || !quizNameElem.value) {
+      // alert('smth is null!');
+      return;
+    }
 
-		let reqObj = {
-			materie_id: Number(materieElem.value),
-			capitol_id: Number(capitolElem.value),
-			name: quizNameElem.value,
-			intrebari: [],
-		};
-		for (let i = 1; i <= qLen; i++) {
-			const elem = document.querySelectorAll(`#f${i} > input`);
-			const sel = document.querySelector(`#f${i} > select`);
-			if (
-				!elem[0].value ||
-				!elem[1].value ||
-				!elem[2].value ||
-				!elem[3].value
-			) {
-				alert("smth is null!");
-				return;
-			}
+    let reqObj = {
+      materie_id: Number(materieElem.value),
+      capitol_id: Number(capitolElem.value),
+      name: quizNameElem.value,
+      intrebari: []
+    };
+    for (let i = 1; i <= qLen; i++) {
+      const elem = document.querySelectorAll(`#f${i} > input`);
+      const sel = document.querySelector(`#f${i} > select`);
+      if (!elem[0].value || !elem[1].value || !elem[2].value || !elem[3].value) {
+        // alert('smth is null!');
+        return;
+      }
 
-			reqObj.intrebari.push({
-				nume: elem[0].value,
-				raspunsuri: [
-					{
-						nume: elem[1].value,
-					},
-					{
-						nume: elem[2].value,
-					},
-					{
-						nume: elem[3].value,
-					},
-				],
-				raspuns_corect: elem[sel.value].value,
-			});
-		} //*/
-		//console.log(reqObj);
-		const url = `${globalVars.apiPrefix}/admin/quiz`;
-		axios
-			.post(url, reqObj, {
-				headers: {
-					Authorization: getCookie("jwt"),
-				},
-			})
-			.then(
-				(res) => {
-					window.location.assign("/");
-				},
-				(err) => {
-					alert("error");
-					console.error(err);
-				}
-			);
-	};
+      reqObj.intrebari.push({
+        nume: elem[0].value,
+        raspunsuri: [
+          {
+            nume: elem[1].value
+          },
+          {
+            nume: elem[2].value
+          },
+          {
+            nume: elem[3].value
+          }
+        ],
+        raspuns_corect: elem[sel.value].value
+      });
+    } //*/
+    //console.log(reqObj);
+    const url = `${globalVars.apiPrefix}/admin/quiz`;
+    axios
+      .post(url, reqObj, {
+        headers: {
+          Authorization: getCookie('jwt')
+        }
+      })
+      .then(
+        res => {
+          window.location.assign('/');
+        },
+        err => {
+          alert('error');
+          console.error(err);
+        }
+      );
+  };
 
-	return (
-		<div>
-			<h1>Mk quizzes</h1>
-			<br />
-			<form action="" onSubmit={handleFormSubmit}>
-				<input id="materie_id" type="number" placeholder="materie_id" />
-				<input id="capitol_id" type="number" placeholder="capitol_id" />
-				<input id="nume-quiz" type="text" placeholder="nume-quiz" />
-				<div className="qq" id="f1">
-					<hr />
-					1.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f2">
-					<hr />
-					2.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f3">
-					<hr />
-					3.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f4">
-					<hr />
-					4.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f5">
-					<hr />
-					5.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f6">
-					<hr />
-					6.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f7">
-					<hr />
-					7.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f8">
-					<hr />
-					8.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<div className="qq" id="f9">
-					<hr />
-					9.
-					<input type="text" />
-					<br />
-					variant1: <input type="text" />
-					<br />
-					variant2: <input type="text" />
-					<br />
-					variant3: <input type="text" />
-					<br />
-					varianta corecta:
-					<select name="ok" id="var-ok">
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-					</select>
-					<hr />
-				</div>
-				<input type="submit" value="submit!" />
-			</form>
-		</div>
-	);
+  return (
+    <div className="EvaluareAdmin">
+      <h1>Adaugă un test de evaluare (quiz)</h1>
+      <br />
+      <p>Pentru a adăuga un nou test de rezolvat te rugăm să completezi formularul de mai jos</p>
+      <br />
+      <form action="" onSubmit={handleFormSubmit}>
+        <input id="materie_id" type="number" placeholder="ID-ul materiei" />
+        <input id="capitol_id" type="number" placeholder="ID-ul capitolului" />
+        <input id="nume-quiz" type="text" placeholder="Numele testului" />
+        <div className="qq" id="f1">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 1</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f2">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 2</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f3">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 3</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f4">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 4</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f5">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 5</b>
+          </p>
+          <input type="text" />
+          <br />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f6">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 6</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f7">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 7</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f8">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 8</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <div className="qq" id="f9">
+          <br />
+          <br />
+          <p className="highlight">
+            <b>Întrebarea 9</b>
+          </p>
+          <input type="text" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 1" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 2" />
+          <br />
+          <input type="text" placeholder="Varianta de răspuns 3" />
+          <br />
+          <b>Selectează varianta corectă:</b>
+          <br />
+          <br />
+          <select name="ok" id="var-ok">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <br />
+          <br />
+        </div>
+        <input type="submit" value="Trimite" />
+      </form>
+    </div>
+  );
 }
