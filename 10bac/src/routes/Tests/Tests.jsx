34ef954b137a2eaa6551ddb1c5a39e9145ca4cf9
StanaAndrei <stadey33@gmail.com>
2021-12-05 02:26:25 +0200

    upload-subs

--- 10bac/src/routes/Tests/Tests.jsx
+++ 10bac/src/routes/Tests/Tests.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./Tests.css";
 import globalVars from "./../../globalVars";
 import axios from "axios";
-import { getCookie, getUserDataFromJwtReq } from "./../../utils";
+import { getCookie } from "./../../utils";
 
 const Tests = () => {
 	const [subDatas, setSubDatas] = React.useState(null);
+	const [pic, setPic] = React.useState(null);
 
 	React.useEffect(() => {
 		const url = `${globalVars.apiPrefix}/user/subs_bac`;
@@ -26,6 +27,35 @@ const Tests = () => {
 			);
 	}, []);
 
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+		const { fid } = event.target.dataset;
+		const url = `${globalVars.apiPrefix}/user/sub_bac/${fid}`;
+        console.log(url);
+        //console.log();
+		const formData = new FormData();
+        console.log(pic);
+		formData.append("file", pic);
+		axios.post(url, formData, {
+			headers: {
+				Authorization: getCookie("jwt"),
+				"Content-Type": "multipart/form-data",
+			},
+		}).then(res => {
+            alert('success!');
+        }, err => {
+            alert('error!');
+            console.error(err);
+        })
+
+	};
+
+	const handleChange = (event) => {
+		event.preventDefault();
+		setPic(event.target.files[0]);
+        console.log(event.target.files[0]);
+	};
+
 	return (
 		<div className="Tests">
 			<h2>Modele de subiecte pentru examenul de bacalaureat</h2>
@@ -42,6 +72,29 @@ const Tests = () => {
 								style={{ width: "800px", height: "800px" }}
 								title="english"
 							></iframe>
+							<br />
+							<form
+								onSubmit={handleFormSubmit}
+								method="POST"
+								enctype="multipart/form-data"
+								data-fid={subData._id}
+							>
+								<label for="file" className="file-label">
+									<input
+										onChange={handleChange}
+										type="file"
+										name="file"
+										id="file"
+									/>
+									Încarcă o fotografie
+								</label>
+								<br />
+								<br />
+								<button type="submit">Trimite</button>
+
+								<br />
+								<br />
+							</form>
 						</div>
 					);
 				})}
